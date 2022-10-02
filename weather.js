8c43d781b086df7a1016e497d5131a067514bf2c
KemalDurukanMERT <kdurukanmert@gmail.com>
2022-10-03 01:36:01 +0200

    functional app

--- weather.js
+++ weather.js
@@ -32,8 +32,35 @@ const getWeatherDataFromApi = async () => {
         const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
         const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
 
-        
 
+        const cityNameSpans = list.querySelectorAll('.city span')
+        const cityNameSpansArray = Array.from(cityNameSpans)
+        if (cityNameSpansArray.length >0){
+            const filteredArray= cityNameSpansArray.filter(span => span.innerText == name)
+            if (filteredArray.length>0){
+                msg.innerText = `You already know the weather for ${name}, Please search for another city,`
+                setTimeout(()=> {
+                    msg.innerText = ''
+                }, 5000)
+                form.reset()
+                return
+            }
+        }
+        const createdLi = document.createElement('li')
+        createdLi.classList.add('city')
+        createdLi.innerHTML = `
+            <h2 class="city-name" data-name="${name}, ${sys.country}">
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+            </h2>
+            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+            <figure>
+                <img class="city-icon" src="${iconUrlAWS}">
+                <figcaption>${weather[0].description}</figcaption>
+            </figure>`
+
+
+        list.prepend(createdLi)
 
     } catch (error) {
         console.log(error);
